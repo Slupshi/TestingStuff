f1cab6d3622f6916afe8705e794ce00d19848ae3
Kevin Morainville <KevinMorainville@isipharm.fr>
2021-11-29 17:27:59 +0100

    Magnets puzzle page 523/789 - use clause From

--- TestingStuff/Program.cs
+++ TestingStuff/Program.cs
@@ -1914,7 +1914,7 @@ namespace TestingStuff
         {
             public static void ChooseLinq()
             {
-                Console.WriteLine("Press 1 for TestLinq, 2 for Comic, 3 for //");
+                Console.WriteLine("Press 1 for TestLinq, 2 for Comic, 3 for MagnetsLinq");
                 Console.WriteLine("Any other key to quit");
                 char linqKey = Char.ToUpper(Console.ReadKey().KeyChar);
                 switch (linqKey)
@@ -1929,7 +1929,7 @@ namespace TestingStuff
                         break;
                     case '3':
                         Console.Clear();
-
+                        LinqFridge.MagnetsLinq();
                         break;
                     default:
                         return;
@@ -1996,6 +1996,30 @@ namespace TestingStuff
                 }
 
             }//Fin de la class Comic
+
+            class LinqFridge
+            {
+                public static void MagnetsLinq()
+                {
+                    //Output : "Get your kicks on route 66"
+
+                    int[] badgers = { 36, 5, 91, 3, 41, 69, 8 };
+
+                    var skunks =
+                        from pigeon in badgers
+                        where (pigeon != 36 && pigeon < 50)
+                        orderby pigeon descending
+                        select pigeon + 5; //Ajoute 5 a chaque nombre de pigeon
+
+                    var bears = skunks.Take(3); // Garde les 3 premiers nombres de Skunks (index : 0,1,2)
+
+                    var weasels =
+                        from sparrow in bears
+                        select sparrow - 1;
+
+                    Console.WriteLine("Get your kicks on route {0}", weasels.Sum());
+                }
+            }//Fin de la class LinqFridge
 
         }//Fin de la class LINQ
 
